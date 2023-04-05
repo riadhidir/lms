173f936659e04196dab775af4d641165c3dfc117
Riadh Idir <riadhidir5@gmail.com>
2023-04-05 10:15:34 +0100

    bug-fix

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,20 @@ import { useState } from 'react'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import ServiceContainer from './components/ServiceContainer'
+import TeamContainer from './components/TeamContainer';
 
-
+import  "./styles/bootstrap.min.css";
+import "./styles/style.css";
 function App() {
   
 
   return (
     <>
-      <Navbar/>
-      <ServiceContainer/>
-     
-      <Footer/>
+      
+      {/* <ServiceContainer/>
+       */}
+     <TeamContainer/>
+      
     </>
     
   )
--- src/components/TeamContainer.jsx
+++ src/components/TeamContainer.jsx
@@ -10,7 +10,7 @@ const TeamContainer = () => {
         <div className="container-xxl py-5">
             <div className="container">
              <Header heading="courses" subHeading="testest"/>
-                <div classNameName="row g-4">
+                <div className="row g-4">
                     <TeamItem instructor="bougabouga1" designation="bouga1" img={team1}/>
                     <TeamItem instructor="bougabouga2" designation="bouga2" img={team2}/>
                     <TeamItem instructor="bougabouga3" designation="bouga3" img={team3}/>
